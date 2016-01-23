10196a846425c0523acd36d8b30531a69c6a250d
tlsmooth88 <tlsmooth88@gmail.com>
2016-01-23 02:48:27 +0000

    edit-profile done with hosting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,6 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) << :location
     devise_parameter_sanitizer.for(:account_update) << :username
     devise_parameter_sanitizer.for(:account_update) << :location
-    # devise_parameter_sanitizer.for(:account_update) << :hosting
+    devise_parameter_sanitizer.for(:account_update) << :hosting
   end
 end
--- db/migrate/20160122122114_add_columns_to_users.rb
+++ db/migrate/20160122122114_add_columns_to_users.rb
@@ -2,6 +2,6 @@ class AddColumnsToUsers < ActiveRecord::Migration
   def change
     add_column :users, :username, :string
     add_column :users, :location, :string
-    add_column :users, :hosting, :boolean
+    add_column :users, :hosting, :boolean, :default => false
   end
 end
