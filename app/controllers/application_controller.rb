0d1228ed387216ad28cffa30f395aed55668a477
tlsmooth88 <tlsmooth88@gmail.com>
2016-01-23 02:18:29 +0000

    location and hosting on users done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,11 @@ class ApplicationController < ActionController::Base
   protected
   
   def configure_permitted_parameters
+    # devise_parameter_sanitizer.for(:sign_up) { |u| u.permit( :username, :location )}
     devise_parameter_sanitizer.for(:sign_up) << :username
     devise_parameter_sanitizer.for(:sign_up) << :location
     devise_parameter_sanitizer.for(:account_update) << :username
     devise_parameter_sanitizer.for(:account_update) << :location
+    # devise_parameter_sanitizer.for(:account_update) << :hosting
   end
 end
