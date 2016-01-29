67c737e9857a8e8e82ee3d8563316ef8fa3cf2c8
tlsmooth88 <tlsmooth88@gmail.com>
2016-01-29 06:51:19 +0000

    uppercase-lowercase correction

--- db/migrate/20160123044356_create_events.rb
+++ db/migrate/20160123044356_create_events.rb
@@ -1,12 +1,12 @@
 class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
-      t.reference :user, index: true #, foreign_key: true
+      t.reference :user, index: true, foreign_key: true
       t.date :start
       t.date :end
 
       t.timestamps null: false
-      t.index [:uer_Id, :created_at]
+      t.index [:uer_id, :created_at]
     end
   end
 end
